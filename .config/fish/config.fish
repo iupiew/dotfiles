774ac9c23b76e2551418b5de3d86a20155825b88
iupiew <iupiew666@gmail.com>
2024-08-06 16:19:13 -0400

    update

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+
+end
+
+alias e="emacs -fs"
+export PATH="$HOME/.cabal/bin:$HOME/.ghcup/bin:$PATH"
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/iupiew/.ghcup/bin $PATH # ghcup-env
