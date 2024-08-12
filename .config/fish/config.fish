b5b6eaffbbf937b709cef2d01440253ef7aeeb18
iupiew <iupiew666@gmail.com>
2024-08-12 07:20:11 -0400

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,8 @@ alias e="emacs -fs"
 export PATH="$HOME/.cabal/bin:$HOME/.ghcup/bin:$PATH"
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/iupiew/.ghcup/bin $PATH # ghcup-env
+
+function fish_prompt
+  # interactive user name @ host name, date/time in YYYY-mm-dd format and path
+  echo (whoami)@(hostname) ">> "
+end
