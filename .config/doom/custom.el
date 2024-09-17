ec17c73bca2cc2a95b739ad22b6464283829a8b9
iupiew <iupiew666@gmail.com>
2024-09-17 08:07:12 -0400

    add svelte support

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("7c28419e963b04bf7ad14f3d8f6655c078de75e4944843ef9522dbecfcd8717d" "30d174000ea9cbddecd6cc695943afb7dba66b302a14f9db5dd65074e70cc744" default))
- '(package-selected-packages '(vue-mode rust-mode)))
+ '(package-selected-packages '(svelte-mode vue-mode rust-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
